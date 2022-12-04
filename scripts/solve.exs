fa02b076c3eb31bdd91df4ee9bea56c4bb158706
Angeline Tamayo <tamayo.angeline@gmail.com>
2022-12-04 16:05:12 -0500

    :hammer: Allow answer outputter script to take multiple inputs

--- scripts/solve.exs
+++ scripts/solve.exs
@@ -1,11 +1,11 @@
 # Outputs the answers to the given day's puzzles, based on your (hopefully correct!) solution functions
-arg =
+args =
   case System.argv() do
-    [day_number | _] ->
-      day_number
-
     [] ->
-      raise "Please pass in the Advent Of Code 2022 day number."
+      raise "Please pass in Advent Of Code 2022 day numbers, space-delimited."
+
+    day_numbers ->
+      day_numbers
   end
 
-IO.inspect AdventOfCode2022.answer(arg)
+Enum.each(args, &IO.inspect(AdventOfCode2022.answer(&1), label: "🌈 Day #{&1}"))
