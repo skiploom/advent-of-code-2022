ecb0d8dd9e3441ff38aba468b484d671ef566b5c
Angeline Tamayo <tamayo.angeline@gmail.com>
2022-12-06 12:44:38 -0500

    :sparkles: Answer day 6

--- lib/advent_of_code_2022/day_6.ex
+++ lib/advent_of_code_2022/day_6.ex
@@ -1,9 +1,51 @@
 defmodule AdventOfCode2022.Day6 do
   use AdventOfCode2022.Solution
 
-  def part_one() do
+  @start_of_packet_marker_length 4
+  @start_of_message_marker_length 14
+
+  @type buffer() :: String.t()
+  @type marker() :: String.t()
+
+  def part_one(), do: find_marker_location(get_buffer(), @start_of_packet_marker_length)
+  def part_two(), do: find_marker_location(get_buffer(), @start_of_message_marker_length)
+
+  @spec find_marker_location(buffer(), expected_marker_length :: integer()) :: integer()
+  def find_marker_location(buffer, marker_length) do
+    {initial_queue, remaining_buffer} = enqueue_from_buffer([], buffer, marker_length)
+    find_marker_location(remaining_buffer, initial_queue, marker_length)
   end
 
-  def part_two() do
+  @typep queue() :: [String.t()]
+
+  @spec find_marker_location(buffer(), queue(), acc :: integer()) :: integer()
+  defp find_marker_location("", queue, acc) do
+    if all_unique?(queue) do
+      acc
+    else
+      raise("No marker found in input buffer.")
+    end
   end
+
+  defp find_marker_location(buffer, queue, acc) do
+    if all_unique?(queue) do
+      acc
+    else
+      {new_queue, remaining_buffer} = enqueue_from_buffer(queue, buffer, 1)
+      find_marker_location(remaining_buffer, new_queue, acc + 1)
+    end
+  end
+
+  @spec enqueue_from_buffer(queue(), buffer(), num_elements_to_enqueue :: integer()) ::
+          {queue(), buffer()}
+  defp enqueue_from_buffer(queue, buffer, num) do
+    {chars_to_enqueue_str, new_buffer} = String.split_at(buffer, num)
+    new_queue = Enum.drop(queue, num) ++ String.split(chars_to_enqueue_str, "", trim: true)
+    {new_queue, new_buffer}
+  end
+
+  defp all_unique?(queue), do: Enum.count(Enum.uniq(queue)) == Enum.count(queue)
+
+  @spec get_buffer() :: String.t()
+  def get_buffer(), do: hd(read_lines!(trim: true))
 end
--- test/day_6_test.exs
+++ test/day_6_test.exs
@@ -3,4 +3,33 @@ defmodule Day6Test do
   alias AdventOfCode2022.Day6
   doctest Day6
 
+  describe "functions correctly solve example inputs on the puzzle website" do
+    test "find_marker_location/2 works for part one" do
+      example_inputs_and_answers = [
+        {"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7},
+        {"bvwbjplbgvbhsrlpgdmjqwftvncz", 5},
+        {"nppdvjthqldpwncqszvftbrmjlhg", 6},
+        {"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10},
+        {"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11}
+      ]
+
+      assert Enum.all?(example_inputs_and_answers, fn {input, answer} ->
+               Day6.find_marker_location(input, 4) == answer
+             end)
+    end
+
+    test "part_two/0" do
+      example_inputs_and_answers = [
+        {"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19},
+        {"bvwbjplbgvbhsrlpgdmjqwftvncz", 23},
+        {"nppdvjthqldpwncqszvftbrmjlhg", 23},
+        {"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29},
+        {"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26}
+      ]
+
+      assert Enum.all?(example_inputs_and_answers, fn {input, answer} ->
+               Day6.find_marker_location(input, 14) == answer
+             end)
+    end
+  end
 end
