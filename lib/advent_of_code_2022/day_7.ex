174e301aa2ee2d7a0af10e5f02dc5375c64d235e
Angeline Tamayo <tamayo.angeline@gmail.com>
2022-12-07 16:41:46 -0500

    :poop: This has to be the messiest answer to Day 7 Part 2 of all time

--- lib/advent_of_code_2022/day_7.ex
+++ lib/advent_of_code_2022/day_7.ex
@@ -5,6 +5,7 @@ defmodule AdventOfCode2022.Day7 do
     read_lines!(trim: true)
     |> process([], %{})
     |> replace()
+    |> Map.values()
     |> Enum.map(&List.flatten/1)
     |> Enum.map(&Enum.sum/1)
     |> Enum.filter(&(&1 < 100_000))
@@ -12,16 +13,22 @@ defmodule AdventOfCode2022.Day7 do
   end
 
   def part_two() do
-  end
+    directory_sizes =
+      read_lines!(trim: true)
+      |> process([], %{})
+      |> replace()
+      |> Enum.map(fn {path, contents} -> {path, Enum.sum(List.flatten(contents))} end)
+      |> Map.new()
 
-  def process() do
-    read_example_lines()
-    |> process([], %{})
-    |> replace()
-    |> Enum.map(&List.flatten/1)
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.filter(&(&1 < 100_000))
-    |> Enum.sum()
+    outermost_directory_size = Map.get(directory_sizes, ["/"])
+    unused_space = 70_000_000 - outermost_directory_size
+    needed_unused_space = 30_000_000
+    target = needed_unused_space - unused_space
+
+    directory_sizes
+    |> Map.values()
+    |> Enum.filter(&(&1 >= target))
+    |> Enum.min()
   end
 
   def process([], _, acc), do: acc
@@ -55,11 +62,11 @@ defmodule AdventOfCode2022.Day7 do
   def do_thing({:file, s}, curr, acc), do: {curr, Map.update(acc, curr, [s], &[s | &1])}
 
   def replace(map) do
-    Enum.map(map, &replace(&1, map))
+    Map.new(Enum.map(map, &replace(&1, map)))
   end
 
-  def replace({path, v}, map) do
-    Enum.map(v, &do_replace(&1, path, map))
+  def replace({path, contents}, map) do
+    {path, Enum.map(contents, &do_replace(&1, path, map))}
   end
 
   def do_replace(str, path, map) when is_binary(str) do
@@ -72,35 +79,5 @@ defmodule AdventOfCode2022.Day7 do
   def get_thingy(dir, path, map) do
     Map.get(map, path ++ [dir])
     |> Enum.map(&do_replace(&1, path ++ [dir], map))
-  end
-
-  def read_example_lines() do
-    """
-    $ cd /
-    $ ls
-    dir a
-    14848514 b.txt
-    8504156 c.dat
-    dir d
-    $ cd a
-    $ ls
-    dir e
-    29116 f
-    2557 g
-    62596 h.lst
-    $ cd e
-    $ ls
-    584 i
-    $ cd ..
-    $ cd ..
-    $ cd d
-    $ ls
-    4060174 j
-    8033020 d.log
-    5626152 d.ext
-    7214296 k
-    """
-    |> String.split("\n", trim: true)
-    |> IO.inspect()
   end
 end
