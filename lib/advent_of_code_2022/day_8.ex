6e0c933387e8e4c8bebc56949bf2fa314c01ec8f
Angeline Tamayo <tamayo.angeline@gmail.com>
2022-12-08 16:28:58 -0500

    :recycle: Clean up day 8 answer

--- lib/advent_of_code_2022/day_8.ex
+++ lib/advent_of_code_2022/day_8.ex
@@ -2,13 +2,12 @@ defmodule AdventOfCode2022.Day8 do
   use AdventOfCode2022.Solution
 
   def part_one() do
-    rows_and_cols = parse_into_rows_and_columns(read_lines!(trim: true))
-    trees = parse_into_flat_list(read_lines!(trim: true))
+    %{tree_height_map: tree_height_map, tree_list: trees} = setup()
 
     Enum.reduce(trees, 0, fn tree, num_visible ->
-      pov = build_tree_pov(tree, rows_and_cols)
+      pov = build_tree_pov(tree, tree_height_map)
 
-      if visible?(tree, pov) do
+      if tree_visible_from_any_direction?(tree, pov) do
         num_visible + 1
       else
         num_visible
@@ -17,11 +16,10 @@ defmodule AdventOfCode2022.Day8 do
   end
 
   def part_two() do
-    rows_and_cols = parse_into_rows_and_columns(read_lines!(trim: true))
-    trees = parse_into_flat_list(read_lines!(trim: true))
+    %{tree_height_map: tree_height_map, tree_list: trees} = setup()
 
     Enum.reduce(trees, 0, fn tree, max_scenic_score ->
-      pov = build_tree_pov(tree, rows_and_cols)
+      pov = build_tree_pov(tree, tree_height_map)
       scenic_score = calculate_scenic_score(calculate_viewing_distances(tree, pov))
 
       if scenic_score > max_scenic_score do
@@ -32,23 +30,37 @@ defmodule AdventOfCode2022.Day8 do
     end)
   end
 
+  def setup() do
+    height_matrix = parse_into_tree_height_matrix(read_lines!(trim: true))
+
+    %{
+      tree_height_map: build_tree_height_map(height_matrix),
+      tree_list: build_flat_tree_list(height_matrix)
+    }
+  end
+
   @doc ~S"""
-      iex> AdventOfCode2022.Day8.parse_into_rows_and_columns(["12", "24"])
+      iex> AdventOfCode2022.Day8.parse_into_tree_height_matrix(["12", "24"])
+      [[1, 2], [2, 4]]
+  """
+  def parse_into_tree_height_matrix(input_lines) do
+    input_lines
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> Enum.map(&Enum.map(&1, fn height -> String.to_integer(height) end))
+  end
+
+  @doc ~S"""
+      iex> AdventOfCode2022.Day8.build_tree_height_map([[1, 2], [2, 4]])
       %{rows: %{0 => [1, 2], 1 => [2, 4]}, columns: %{0 => [1, 2], 1 => [2, 4]}}
   """
-  def parse_into_rows_and_columns(input_lines) do
-    tree_matrix =
-      input_lines
-      |> Enum.map(&String.split(&1, "", trim: true))
-      |> Enum.map(&Enum.map(&1, fn height -> String.to_integer(height) end))
-
+  def build_tree_height_map(height_matrix) do
     rows =
-      tree_matrix
+      height_matrix
       |> Enum.with_index(fn heights, row_num -> {row_num, heights} end)
       |> Map.new()
 
     columns =
-      tree_matrix
+      height_matrix
       |> Enum.flat_map(&Enum.with_index(&1, fn height, col_num -> {col_num, height} end))
       |> Enum.reduce(%{}, fn {col_num, height}, acc ->
         Map.update(acc, col_num, [height], fn existing_heights -> existing_heights ++ [height] end)
@@ -58,16 +70,11 @@ defmodule AdventOfCode2022.Day8 do
   end
 
   @doc ~S"""
-      iex> AdventOfCode2022.Day8.parse_into_flat_list(["12", "24"])
+      iex> AdventOfCode2022.Day8.build_flat_tree_list([[1, 2], [2, 4]])
       [%{row: 0, column: 0, height: 1}, %{row: 0, column: 1, height: 2}, %{row: 1, column: 0, height: 2}, %{row: 1, column: 1, height: 4}]
   """
-  def parse_into_flat_list(input_lines) do
-    tree_matrix =
-      input_lines
-      |> Enum.map(&String.split(&1, "", trim: true))
-      |> Enum.map(&Enum.map(&1, fn height -> String.to_integer(height) end))
-
-    tree_matrix
+  def build_flat_tree_list(height_matrix) do
+    height_matrix
     |> Enum.with_index(fn height, row_num -> {height, row_num} end)
     |> Enum.flat_map(fn {row, row_num} ->
       Enum.with_index(row, fn height, col_num ->
@@ -77,38 +84,6 @@ defmodule AdventOfCode2022.Day8 do
   end
 
   def build_tree(col_num, row_num, height), do: %{column: col_num, row: row_num, height: height}
-
-  def calculate_scenic_score(distances), do: Enum.reduce(distances, fn x, acc -> x * acc end)
-
-  def calculate_viewing_distances(tree, pov) do
-    Enum.map(pov, &calculate_viewing_distance(tree, &1, 0))
-  end
-
-  @doc ~S"""
-      iex> AdventOfCode2022.Day8.calculate_viewing_distance(%{height: 2}, [], 0)
-      0
-
-      iex> AdventOfCode2022.Day8.calculate_viewing_distance(%{height: 2}, [1, 0, 1], 0)
-      3
-
-      iex> AdventOfCode2022.Day8.calculate_viewing_distance(%{height: 2}, [1, 2, 1], 0)
-      2
-
-      iex> AdventOfCode2022.Day8.calculate_viewing_distance(%{height: 2}, [2, 0, 1], 0)
-      1
-
-      iex> AdventOfCode2022.Day8.calculate_viewing_distance(%{height: 2}, [1, 0, 2], 0)
-      3
-  """
-  def calculate_viewing_distance(_, [], distance), do: distance
-
-  def calculate_viewing_distance(tree, [head | rest] = _pov, distance) do
-    if tree.height > head do
-      calculate_viewing_distance(tree, rest, distance + 1)
-    else
-      distance + 1
-    end
-  end
 
   @doc ~S"""
   Returns a list of lists,
@@ -121,7 +96,7 @@ defmodule AdventOfCode2022.Day8 do
   There are four lists, which represent the four directions from which you can view trees: north, east, south, and west.
 
       iex> tree = %{row: 0, column: 1, height: 2}
-      ...> rows_and_cols = AdventOfCode2022.Day8.parse_into_rows_and_columns(["123", "456", "789"])
+      ...> rows_and_cols = AdventOfCode2022.Day8.build_tree_height_map([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
       ...> AdventOfCode2022.Day8.build_tree_pov(tree, rows_and_cols)
       [[], [3], [5, 8], [1]]
   """
@@ -134,27 +109,27 @@ defmodule AdventOfCode2022.Day8 do
     [Enum.reverse(up_trees), right_trees, down_trees, Enum.reverse(left_trees)]
   end
 
-  def visible?(tree, pov) do
-    Enum.any?(pov, &visible?(tree, &1, true))
+  def tree_visible_from_any_direction?(tree, pov), do: Enum.any?(pov, &tree_visible?(tree, &1))
+
+  def tree_visible?(tree, heights, is_visible \\ true)
+  def tree_visible?(_, _, false), do: false
+  def tree_visible?(_, [], is_visible), do: is_visible
+  def tree_visible?(tree, [head | rest], _), do: tree_visible?(tree, rest, tree.height > head)
+
+  def calculate_scenic_score(distances), do: Enum.reduce(distances, fn x, acc -> x * acc end)
+
+  def calculate_viewing_distances(tree, pov) do
+    Enum.map(pov, &calculate_viewing_distance(tree, &1))
   end
 
-  @doc ~S"""
-      iex> AdventOfCode2022.Day8.visible?(%{height: 2}, [1, 0, 1], true)
-      true
+  def calculate_viewing_distance(tree, heights, distance \\ 0)
+  def calculate_viewing_distance(_, [], distance), do: distance
 
-      iex> AdventOfCode2022.Day8.visible?(%{height: 2}, [1, 2, 1], true)
-      false
-
-      iex> AdventOfCode2022.Day8.visible?(%{height: 2}, [2, 0, 1], true)
-      false
-
-      iex> AdventOfCode2022.Day8.visible?(%{height: 2}, [1, 0, 2], true)
-      false
-  """
-  def visible?(_, _, false), do: false
-  def visible?(_, [], is_visible), do: is_visible
-
-  def visible?(tree, [head | rest] = _pov, _is_visible) do
-    visible?(tree, rest, tree.height > head)
+  def calculate_viewing_distance(tree, [head | rest], distance) do
+    if tree.height > head do
+      calculate_viewing_distance(tree, rest, distance + 1)
+    else
+      distance + 1
+    end
   end
 end
--- test/day_8_test.exs
+++ test/day_8_test.exs
@@ -3,4 +3,35 @@ defmodule Day8Test do
   alias AdventOfCode2022.Day8
   doctest Day8
 
+  describe "tree_visible?/2" do
+    test "true when other trees are shorter" do
+      assert Day8.tree_visible?(%{height: 2}, [1, 0, 1])
+    end
+
+    test "false when there's a taller tree" do
+      refute Day8.tree_visible?(%{height: 2}, [1, 3, 1])
+    end
+
+    test "false when there's a tree that's the same height" do
+      refute Day8.tree_visible?(%{height: 2}, [1, 2, 1])
+    end
+
+    test "can detect blocking trees even if they're right next your tree, or all the way at the edge" do
+      refute Day8.tree_visible?(%{height: 2}, [1, 0, 2])
+      refute Day8.tree_visible?(%{height: 2}, [2, 0, 1])
+    end
+  end
+
+  describe "calculate_viewing_distance/2" do
+    test "returns 0 if you're at the edge" do
+      assert Day8.calculate_viewing_distance(%{height: 2}, []) == 0
+    end
+
+    test "returns the number of trees you can see until you hit a same-height or taller tree" do
+      assert Day8.calculate_viewing_distance(%{height: 2}, [1, 0, 1]) == 3
+      assert Day8.calculate_viewing_distance(%{height: 2}, [1, 2, 1]) == 2
+      assert Day8.calculate_viewing_distance(%{height: 2}, [2, 0, 1]) == 1
+      assert Day8.calculate_viewing_distance(%{height: 2}, [1, 0, 2]) == 3
+    end
+  end
 end
