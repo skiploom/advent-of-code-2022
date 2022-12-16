e1ec62de927cfa2104247f6bb3701dfd0a5c180c
Angeline Tamayo <tamayo.angeline@gmail.com>
2022-12-16 11:16:47 -0500

    :sparkles: Answer day 13 part 2

--- lib/advent_of_code_2022/day_13.ex
+++ lib/advent_of_code_2022/day_13.ex
@@ -1,6 +1,9 @@
 defmodule AdventOfCode2022.Day13 do
   use AdventOfCode2022.Solution
 
+  @divider_packet_1 [[2]]
+  @divider_packet_2 [[6]]
+
   @type packet_data :: integer() | [integer()]
   @type packet :: [packet_data()]
   @type pair :: {packet(), packet()}
@@ -12,8 +15,14 @@ defmodule AdventOfCode2022.Day13 do
   end
 
   def part_two() do
+    read_lines!(trim: true)
+    |> parse_into_packets()
+    |> Enum.concat([@divider_packet_1, @divider_packet_2])
+    |> Enum.sort(&pair_ordered?/2)
+    |> get_decoder_key()
   end
 
+  @spec sum_correct_indices([pair()]) :: integer()
   def sum_correct_indices(pairs) do
     sum_correct_indices(pairs, 1, 0)
   end
@@ -31,6 +40,7 @@ defmodule AdventOfCode2022.Day13 do
     sum_correct_indices(rest, index + 1, new_acc)
   end
 
+  @spec pair_ordered?(left :: packet(), right :: packet()) :: boolean()
   def pair_ordered?([], []), do: true
   def pair_ordered?([], [_ | _]), do: true
   def pair_ordered?([_ | _], []), do: false
@@ -43,6 +53,7 @@ defmodule AdventOfCode2022.Day13 do
     end
   end
 
+  @spec compare(left :: packet_data(), right :: packet_data()) :: :lt | :gt | :eq
   def compare(left, right) when is_integer(left) and is_integer(right) do
     cond do
       left < right -> :lt
@@ -51,60 +62,57 @@ defmodule AdventOfCode2022.Day13 do
     end
   end
 
-  def compare(left, right) when is_list(left) and is_list(right) do
-    compare_lists(left, right)
-  end
+  def compare(left, right) when is_list(left) and is_list(right), do: compare_lists(left, right)
+  def compare(left, right) when is_integer(left), do: compare_lists([left], right)
+  def compare(left, right) when is_integer(right), do: compare_lists(left, [right])
 
-  def compare(left, right) do
-    cond do
-      is_integer(left) ->
-        compare_lists([left], right)
-
-      is_integer(right) ->
-        compare_lists(left, [right])
-    end
-  end
-
+  @spec compare_lists(left :: packet_data(), right :: packet_data()) :: :lt | :gt | :eq
   def compare_lists([], []), do: :eq
   def compare_lists([], [_ | _] = _right_is_nonempty), do: :lt
   def compare_lists([_ | _] = _left_is_nonempty, []), do: :gt
 
-  def compare_lists([l | tail_l], [r | tail_r]) when is_integer(l) and is_integer(r) do
-    cond do
-      l < r -> :lt
-      l > r -> :gt
-      l == r -> compare_lists(tail_l, tail_r)
-    end
-  end
-
-  def compare_lists([l | tail_l], [r | tail_r]) when is_list(l) and is_list(r) do
-    case compare(l, r) do
-      :lt -> :lt
-      :gt -> :gt
-      :eq -> compare(tail_l, tail_r)
-    end
-  end
-
-  def compare_lists([l | tail_l], [r | tail_r]) do
-    case compare(l, r) do
+  def compare_lists([left | tail_l], [right | tail_r]) do
+    case compare(left, right) do
       :lt -> :lt
       :gt -> :gt
       :eq -> compare_lists(tail_l, tail_r)
     end
   end
 
-  # Parsing functions
+  @spec get_decoder_key([packet()]) :: integer()
+  def get_decoder_key(packets), do: get_decoder_key(packets, 1, nil, nil)
 
-  def parse_into_pairs(input_lines) do
-    Enum.reverse(parse(input_lines, []))
+  def get_decoder_key(_, _, i1, i2) when not (is_nil(i1) or is_nil(i2)), do: i1 * i2
+
+  def get_decoder_key([packet | rest], index, divider1_index, divider2_index) do
+    case packet do
+      @divider_packet_1 -> get_decoder_key(rest, index + 1, index, divider2_index)
+      @divider_packet_2 -> get_decoder_key(rest, index + 1, divider1_index, index)
+      _ -> get_decoder_key(rest, index + 1, divider1_index, divider2_index)
+    end
   end
 
-  def parse([], acc), do: acc
+  # Parsing functions
 
-  def parse([left, right | rest], acc) do
-    parse(rest, [pair(left, right) | acc])
+  @spec parse_into_pairs([String.t()]) :: [pair()]
+  def parse_into_pairs(input_lines) do
+    Enum.reverse(parse_into_pairs(input_lines, []))
+  end
+
+  def parse_into_pairs([], acc), do: acc
+
+  def parse_into_pairs([left, right | rest], acc) do
+    parse_into_pairs(rest, [pair(left, right) | acc])
   end
 
   # Yes, I am very, very lazy.
-  def pair(l, r), do: {Code.string_to_quoted!(l), Code.string_to_quoted!(r)}
+  def pair(l, r), do: {packet(l), packet(r)}
+  def packet(str), do: Code.string_to_quoted!(str)
+
+  @doc "Unlike parse_into_pairs/1, this will return the packets in reverse order. (Doesn't matter for the puzzle.)"
+  @spec parse_into_packets([String.t()]) :: [packet()]
+  def parse_into_packets(input_lines), do: parse_into_packets(input_lines, [])
+
+  def parse_into_packets([], acc), do: acc
+  def parse_into_packets([line | rest], acc), do: parse_into_packets(rest, [packet(line) | acc])
 end
